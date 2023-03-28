5e86289f368437fb2c83b8838612ff88a9360b76
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-03-28 14:40:59 +0200

    Update scripts.js

--- IWA2/src/js/scripts.js
+++ IWA2/src/js/scripts.js
@@ -3,10 +3,10 @@
 
 /*This is the firstname of the user*/
 
-const user = 'John';
+const user = "John";
 
 /* This is the lastname of the user */
-const surname = 'Smith';
+const surname = "Smith";
 
 console.log(user, surname)
 
@@ -18,7 +18,7 @@ console.log(user, surname)
 /*
  * This is the date that a user created their account 
  */
-const date = '10/07/2014'
+const date = "10/07/2014"
 console.log(date)
 
 
