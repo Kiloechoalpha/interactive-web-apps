2ccdb14596d2f8f51146f7bcaa96669e6a1f5c7c
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-03-29 22:49:32 +0200

    format files

--- IWA3/src/js/scripts.js
+++ IWA3/src/js/scripts.js
@@ -1,14 +1,12 @@
-// CHALLENGE 1
-import {company} from './configuration.js';
-import {year} from './configuration.js';
+import { company } from './configuration.js';
+import { year } from './configuration.js';
 
 const message = '© ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
 
-// CHALLENGE 2
-import {role1} from './nwabisa.js';
-import {role2} from './johannes.js';
-import {role3} from './alex.js';
+import { role1 } from './nwabisa.js';
+import { role2 } from './johannes.js';
+import { role3 } from './alex.js';
 console.log('Roles:', role1, role2, role3)
 
 
--- IWA4/src/js/scripts.js
+++ IWA4/src/js/scripts.js
@@ -1,39 +1,39 @@
-import {company} from './configuration.js';
-import {year} from './configuration.js';
+import { company } from './configuration.js';
+import { year } from './configuration.js';
 
 const message = '© ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
 
 let date = '2050';
 let rank = "parent";
-let count = 0 ;
+let count = 0;
 
 if (date = "2050") {
-	console.log("January", 'New Year’s Day')
-	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	 count += 4
+    console.log("January", 'New Year’s Day')
+    console.log("March", 'Human Rights Day')
+    date = 'April'
+    console.log(date, 'Family Day')
+    console.log(date, 'Freedom Day')
+    count += 4
 
-	if (rank = false ) {
-	  console.log('June', 'Youth Day')
-	  count += 1
-  }
+    if (rank = false) {
+        console.log('June', 'Youth Day')
+        count += 1
+    }
 
-	console.log('August', 'Women’s Day')
-	console.log('September', 'Heritage Day')
-	date = 'December'
-	console.log(date, 'Day of Reconciliation')
-	count += 3
+    console.log('August', 'Women’s Day')
+    console.log('September', 'Heritage Day')
+    date = 'December'
+    console.log(date, 'Day of Reconciliation')
+    count += 3
 
-	if (rank ="parent" ) {
-	  console.log(date, 'Christmas Day')
-	  count += 1
-  }
+    if (rank = "parent") {
+        console.log(date, 'Christmas Day')
+        count += 1
+    }
 
-	console.log(date, 'Day of Goodwill')
-	count += 1
+    console.log(date, 'Day of Goodwill')
+    count += 1
 }
 date = '2050'
 
