9f3b98f33567c314e8b8605d58b4ff283881562b
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-03-29 13:54:39 +0200

    Create Challenge 1 solutions

---
+++ IWA3/src/js/scripts.js
@@ -0,0 +1,10 @@
+// CHALLENGE 2
+import {company} from './configuration.js';
+import {year} from './configuration.js';
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message
+
+// CHALLENGE 2
+console.log('Roles:', nwabisa.role, johanes.role, alex.role)
+
