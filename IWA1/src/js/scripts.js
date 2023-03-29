48448550e18a426c6c8e2b9b5c2a1638d91e2e27
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-03-29 10:54:46 +0200

    update scripts

--- IWA1/src/js/scripts.js
+++ IWA1/src/js/scripts.js
@@ -1,12 +1,11 @@
-const MAX_NUMBER = 15
-const MIN_NUMBER = -5
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
 const STEP_AMOUNT = 5;
-
 
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
-const add = document.querySelector('[data-key="add"]')
+const add = document.querySelector('[data-key="add"]') 
 
 
 const subtractHandler = () => {
