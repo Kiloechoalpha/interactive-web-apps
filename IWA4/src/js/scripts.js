28177c171e2b2a52332ab79fc5cf0fded9e24544
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-03-29 22:44:28 +0200

    Update scripts.js

--- IWA4/src/js/scripts.js
+++ IWA4/src/js/scripts.js
@@ -5,8 +5,8 @@ const message = '© ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
 
 let date = '2050';
-let rank = "student";
-let count = "0";
+let rank = "parent";
+let count = 0 ;
 
 if (date = "2050") {
 	console.log("January", 'New Year’s Day')
@@ -14,26 +14,26 @@ if (date = "2050") {
 	date = 'April'
 	console.log(date, 'Family Day')
 	console.log(date, 'Freedom Day')
-	 count = count + 4
+	 count += 4
 
-	if (rank = "student" ) {
+	if (rank = false ) {
 	  console.log('June', 'Youth Day')
-	  count = count + 1
+	  count += 1
   }
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
 	date = 'December'
 	console.log(date, 'Day of Reconciliation')
-	count = count + 3
+	count += 3
 
-	if (rank = "parent") {
+	if (rank ="parent" ) {
 	  console.log(date, 'Christmas Day')
-	  count = count + 1
+	  count += 1
   }
 
 	console.log(date, 'Day of Goodwill')
-	count = count + 1
+	count += 1
 }
 date = '2050'
 
