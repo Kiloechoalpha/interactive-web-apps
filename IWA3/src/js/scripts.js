f77f4a5e64767632d89d9791ae9b2aab0bdce3cb
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-04-03 09:51:20 +0200

    Update scripts.js

--- IWA3/src/js/scripts.js
+++ IWA3/src/js/scripts.js
@@ -11,7 +11,7 @@ document.querySelector('footer').innerText = message
 import { role1 } from './nwabisa.js';
 import { role2 } from './johannes.js';
 import { role3 } from './alex.js';
-console.log('Roles:'+ role1+","+ role2+", "+ role3)
+console.log('Roles:'+ role1+", "+ role2+", "+ role3)
 
 
 
