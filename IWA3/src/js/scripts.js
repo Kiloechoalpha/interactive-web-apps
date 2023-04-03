6d2fccd77480e0da2449b56913cd139cd95ed159
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-04-03 09:01:15 +0200

    Update links

--- IWA3/src/js/scripts.js
+++ IWA3/src/js/scripts.js
@@ -1,8 +1,12 @@
+//IWA3 Challenge 1
+
 import { company } from './configuration.js';
 import { year } from './configuration.js';
 
 const message = '© ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
+
+//IWA4 Challenge 1
 
 import { role1 } from './nwabisa.js';
 import { role2 } from './johannes.js';
