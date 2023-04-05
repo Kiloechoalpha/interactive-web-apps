16e70862f33f0507f25ce7feb6aa0f7a08eee76c
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-04-05 09:34:21 +0200

    Update scripts.js

--- IWA6/src/JS/scripts.js
+++ IWA6/src/JS/scripts.js
@@ -1,18 +1,13 @@
-
-
-
 // Challenge 6.1
 const primaryPhone = 'O748105141'
 const secondaryPhone = '0219131568'
 
-// Only change below this line------------------------------------------------------------------------------------------------------------------
-
+// Only change below this line -------------------------------------------------------------------------
   const primaryValid = /^\d+$/.test(primaryPhone );
   const secondaryValid = /^\d+$/.test(secondaryPhone );
   
   console.log('Primary phone is valid numerical string:', primaryValid)
   console.log('Secondary phone is valid numerical string:', secondaryValid )
-
 
 // Challenge 6.2
 
@@ -24,10 +19,9 @@ const transport = 10.2;
 const hourOfDay = 00;
 const minuteOfDay = 00;
 
-// Only change below this line-------------------------------------------------------------------------------------------------------------------
-
+// Only change below this line -------------------------------------------------------------------------
 if (hourOfDay !== undefined && minuteOfDay !== undefined && hourOfDay === 00 && minuteOfDay === 00) {
-	const taxAsDecimal = parseFloat(tax) / 100;
+	const taxAsDecimal = parseInt(tax) / 100;
     const startingAfterTax = salary * (1 - taxAsDecimal);
    const balance = startingAfterTax - transport - food - rent;
 
