056dca17f72bd10acdf9f9215995cc7ab3614c47
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-04-05 10:00:25 +0200

    Create IWA7

---
+++ IWA7/src/js/scripts.js
@@ -0,0 +1,21 @@
+//Challenge 7.1
+const value = 3
+console.log(value + 4 + value)
+
+//Challenge 7.2
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+if (nickname === undefined) {
+    console.log("Good Morning, "+ firstname + "!")
+}
+
+console.log("Good Morning, ${nickname} || {firstname}!")
+
+
+
+
+/*
+//Challenge 7.3
+
+*/
