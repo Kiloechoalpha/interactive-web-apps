ce0be2cdb6185f3b2ef8a2c136e66b2298a9fb96
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-04-05 16:35:48 +0200

    Update scripts.js

--- IWA7/src/js/scripts.js
+++ IWA7/src/js/scripts.js
@@ -42,22 +42,23 @@ console.log("Good Morning,  ${nickname} || ${firstname" )
 
 //Challenge 7.3
 
-const leoName = 'Leo';
-const leoSurname = 'Musvaire     ';
-const leoBalance = '-9394';
+const leoName = 'Leo'
+const leoSurname = 'Musvaire      '
+const leoBalance = '-9394'
 
-const sarahName = 'Sarah    ';
-const sarahSurname = 'Kleinhans';
-const sarahBalance = '-4582.21000111';
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
 
-const divider = '----------------------------------';
+const divider = '----------------------------------'
 
-// Only change below this line
+// Only change below this line--
 
-const owed = parseFloat(leoBalance) + parseFloat(sarahBalance);
-const leo = `${leoName} ${leoSurname} (Owed: R ${parseFloat(leoBalance).toFixed(2)})\n`;
-const sarah = `${sarahName} ${sarahSurname} (Owed: R ${parseFloat(sarahBalance).toFixed(2)})\n`;
+const owed = parseInt(-1 * leoBalance) + parseInt(-1 * sarahBalance);
+const leo = `${leoName} ${leoSurname } (Owed: R ${parseInt(-leoBalance).toFixed(2)})\n`;
+const sarah = `${sarahName} ${sarahSurname} (Owed: R ${parseInt(-sarahBalance).toFixed(2)})\n`;
 const total = `  Total amount owed: R ${owed.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ")}`;
 const result = `\n${leo}${sarah}${divider}\n${total}\n${divider}`;
 
 console.log(result);
+
