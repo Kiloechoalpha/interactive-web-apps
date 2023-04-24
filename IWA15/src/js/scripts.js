b7db1b54e4d1086b80e7af534668801f20314e92
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-04-24 15:11:26 +0200

    update IWA15

--- IWA15/src/js/scripts.js
+++ IWA15/src/js/scripts.js
@@ -8,27 +8,50 @@ const data = {
     ]
     }
     
-    // Only edit below
+// Only edit bellow
     
-    const first = data.lists[0][1]
-    const second = data.lists[1][1]
-    const third = data.lists[2][1]
-    
-    const result = []
-    
-    const extractBiggest = () => {
-    if (first[first.length - 1] >= second[second.length - 1] && first[first.length - 1] >= third[third.length - 1]) {
-    return first.pop()
-    }
-    
-  
+const first = data.lists[0][1];
+const second = data.lists[1][1];
+const third = data.lists[2][1];
 
-    if (second[second.length - 1] >= first[first.length - 1] && second[second.length - 1] >= third[third.length - 1]) {
-        return second.pop()
-    }
+const result = [];
+
+const extractBiggest = () => {
+if (first.length > 0 && (second.length == 0 || first[first.length - 1] > second[second.length - 1]) && (third.length == 0 || first[first.length - 1] > third[third.length - 1])) {
+return first.pop();
+} else if (second.length > 0 && (third.length == 0 || second[second.length - 1] > third[third.length - 1])) {
+return second.pop();
+} else if (third.length > 0) {
+return third.pop();
+}
+};
+    // const  {
+    //     lists: [
+    //     [, first],
+    //     [, second],
+    //     [, third],
+    //     ]
+    //     } = data
     
-    return third.pop()
-    }
+
+    // const result = []
+    
+    // const extractBiggest = () => {
+    //     const firstLast = first.at (-1)
+    //     const secondLast = second.at (-1)
+    //     const thirdLast = third.at (-1)
+    // if (first[first.length - 1] > second[second.length - 1] > third[third.length - 1]) {
+    //     return first.pop()
+    //     }
+
+
+    //    if (second[second.length - 1] > third[third.length - 1]) {
+    //           return second.pop()
+    //     }
+         
+    // return third.pop()
+    // }
+
     
     // Only edit above
     
